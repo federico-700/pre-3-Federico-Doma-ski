0815bd51c40e1e2c59cb5ff8309bb0c73b57ebc9
Federico Domañski <fededoma7@gmail.com>
2024-09-12 11:22:43 -0300

    login-logout

--- proyecto/core/urls.py
+++ proyecto/core/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 
 from . import views
 
@@ -6,4 +7,6 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login', LoginView.as_view(template_name='core/login.html'), name='login'),
+    path('logout', LogoutView.as_view(template_name='core/logout.html'), name='logout'),
 ]
--- proyecto/proyecto/settings.py
+++ proyecto/proyecto/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+from django.urls import reverse_lazy
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -129,3 +129,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# configuracion de autenticacion
+LOGIN_URL = reverse_lazy('core:login')              # URL de la pagina de inicio de sesion
+LOGIN_REDIRECT_URL = reverse_lazy('core:index')     # URL a la que se redirige despues del inicio de sesion
+
+
